af791317f812ea173c04472b7d1b61c2f3038672
William Crownover <williamcrownover1@gmail.com>
2023-01-09 20:23:27 -0800

    Simple request, broken by CORS

--- src/components/SteamUser.tsx
+++ src/components/SteamUser.tsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import { getUserGames } from '../utils/api'
+
 export const SteamUser = () => {
+	const [ userData, setUserData ] = useState();
+
+	useEffect(() => {
+		const getData = async () => {
+			setUserData( await getUserGames());
+		};
+
+		getData();
+	})
+
+	useEffect(() => {
+		console.log('Show me the data', userData)
+	}, [userData])
+	
 	return (
 		<h1>Test</h1>
 	)
