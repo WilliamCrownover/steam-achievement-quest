d639892fb9c2f46700e63cfdb7cc532e52bb9215
William Crownover <williamcrownover1@gmail.com>
2023-01-22 01:01:42 -0800

    Display distribution of difficulty in a bar graph. Moved game title to below achievements.

--- frontend/src/components/SteamUser.js
+++ frontend/src/components/SteamUser.js
@@ -14,15 +14,15 @@ export const SteamUser = () => {
 			const gamesWithoutAchievementsData = allData.filter(game => game.achievements === undefined);
 			// const sorted = data.sort((a,b) => a.name.localeCompare(b.name)).sort((a,b) => b.playtime_forever-a.playtime_forever);
 			// gamesWithAchievementsData.sort((a, b) => b.achievements[b.achievements.length - 1].percent - a.achievements[a.achievements.length - 1].percent).sort((a, b) => a.achievements.length - b.achievements.length);
-			gamesWithAchievementsData
-			.sort((a, b) => b.achievementDifficultyDistribution.impossiblePercent - a.achievementDifficultyDistribution.impossiblePercent)
-			.sort((a, b) => b.achievementDifficultyDistribution.hardPercent - a.achievementDifficultyDistribution.hardPercent)
-			.sort((a, b) => b.achievementDifficultyDistribution.mediumPercent - a.achievementDifficultyDistribution.mediumPercent)
-			.sort((a, b) => b.achievementDifficultyDistribution.easyPercent - a.achievementDifficultyDistribution.easyPercent);
+			gamesWithAchievementsData.sort((a, b) => b.achievements[b.achievements.length - 1].percent - a.achievements[a.achievements.length - 1].percent);
+			// gamesWithAchievementsData
+			// 	.sort((a, b) => b.achievementDifficultyDistribution.impossiblePercent - a.achievementDifficultyDistribution.impossiblePercent)
+			// 	.sort((a, b) => b.achievementDifficultyDistribution.hardPercent - a.achievementDifficultyDistribution.hardPercent)
+			// 	.sort((a, b) => b.achievementDifficultyDistribution.mediumPercent - a.achievementDifficultyDistribution.mediumPercent)
+			// 	.sort((a, b) => b.achievementDifficultyDistribution.easyPercent - a.achievementDifficultyDistribution.easyPercent);
 			gamesWithoutAchievementsData.sort((a, b) => a.name.localeCompare(b.name));
 			setGameWithAchievements(gamesWithAchievementsData);
 			setGameWithoutAchievements(gamesWithoutAchievementsData);
-			console.log(gamesWithAchievementsData);
 			setLoading(false);
 		};
 
@@ -61,20 +61,32 @@ export const SteamUser = () => {
 		)
 	}
 
+	const displayGraph = (achievementList) => {
+		return (
+			<div className='completeGraph'>
+				{achievementList.map((achievement) => {
+					const percent = achievement.percent.toFixed(2);
+					const colorFill = setColorFill(percent);
+					return (
+						<div style={{ width: `${1 / achievementList.length * 100}%`, height: `${achievement.percent*2}px`, backgroundColor: `${achievement.achieved ? 'lightGrey' : colorFill}` }} />
+					)
+				})}
+			</div>
+		)
+	}
+
 	const displayAchievements = (achievementList) => {
-			return (
-				<>
-					<div className='achievementList'>
-						{achievementList.map((achievement) => {
-							const name = achievement.name;
-							const percent = achievement.percent.toFixed(2);
-							const colorFill = setColorFill(percent);
-							const achieved = achievement.achieved && 'achieved'
-							return <h3 key={name} title={name} style={{ backgroundColor: colorFill }} className={achieved}>{percent}</h3>
-						})}
-					</div>
-				</>
-			)
+		return (
+			<div className='achievementList'>
+				{achievementList.map((achievement) => {
+					const name = achievement.name;
+					const percent = achievement.percent.toFixed(2);
+					const colorFill = setColorFill(percent);
+					const achieved = achievement.achieved && 'achieved'
+					return <h3 key={name} title={name} style={{ backgroundColor: colorFill }} className={achieved}>{percent}</h3>
+				})}
+			</div>
+		)
 	}
 
 	return (
@@ -87,22 +99,23 @@ export const SteamUser = () => {
 						const distribution = game.achievementDifficultyDistribution;
 						return (
 							<div key={game.appid}>
-								{displayTitleInfo(game)}
-								<h3>{game.totalAchievements} Total Achievements</h3>
-								<h3>{game.totalCompletedAchievements} Completed - {game.percentComplete}%</h3>
+								{displayGraph(game.achievements)}
 								<div className='difficultyBar'>
-									<div className='segment easy' style={{width: `${distribution.easyPercent}%`}}/>
-									<div className='segment medium' style={{width: `${distribution.mediumPercent}%`}}/>
-									<div className='segment hard' style={{width: `${distribution.hardPercent}%`}}/>
-									<div className='segment impossible' style={{width: `${distribution.impossiblePercent}%`}}/>
+									<div className='segment easy' style={{ width: `${distribution.easyPercent}%` }} />
+									<div className='segment medium' style={{ width: `${distribution.mediumPercent}%` }} />
+									<div className='segment hard' style={{ width: `${distribution.hardPercent}%` }} />
+									<div className='segment impossible' style={{ width: `${distribution.impossiblePercent}%` }} />
 								</div>
 								<h3
 									className={`${game.percentComplete === '100.00' && 'achieved'}`}
-									style={{ backgroundColor: setColorFill(game.averagePercent) }}
+									style={{ backgroundColor: setColorFill(game.averagePercent), margin: '3px 0px' }}
 								>
 									{game.averagePercent}
 								</h3>
 								{displayAchievements(game.achievements)}
+								{displayTitleInfo(game)}
+								<h3>{game.totalAchievements} Total Achievements</h3>
+								<h3 className='bottomItem'>{game.totalCompletedAchievements} Completed - {game.percentComplete}%</h3>
 							</div>
 						)
 					})}
