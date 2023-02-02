a898b1eeeb8743d1a6d3d5620b5bd85ec96f6ac6
William Crownover <williamcrownover1@gmail.com>
2023-02-02 01:27:19 -0800

    Styled pills for dlc and not played flags

--- frontend/src/components/GameTitleInfo.js
+++ frontend/src/components/GameTitleInfo.js
@@ -1,11 +1,15 @@
 export const GameTitleInfo = ({ game }) => {
-	const hoursPlayed = game.hoursPlayed;
+	const {
+		hoursPlayed,
+		lastPlayedDate,
+	} = game;
 
 	return (
 		<div className='gameTitleInfo'>
 			<a className='gameTitleLink' href={game.gameUrl} target='_blank' rel='noreferrer'>{game.name}</a>
+			{game.has_dlc && <p className='dlc'>+DLC</p>}
 			{hoursPlayed > 0 && <p>{`${hoursPlayed} Hours Played`}</p>}
-			<p>{game.lastPlayedDate}</p>
+			<p className={`${lastPlayedDate === 'Not Played' && 'notPlayed'}`}>{lastPlayedDate}</p>
 		</div>
 	)
 }
