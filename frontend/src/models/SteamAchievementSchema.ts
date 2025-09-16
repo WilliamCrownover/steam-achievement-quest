75dfe65357fd2b530e024c4b0b7d221b4b4a575b
William Crownover <williamcrownover1@gmail.com>
2025-09-16 09:41:03 -0700

    Updated SteamAchievementSchema model

--- frontend/src/models/SteamAchievementSchema.ts
+++ frontend/src/models/SteamAchievementSchema.ts
@@ -1,8 +1,8 @@
 export interface SteamAchievementSchema extends Record<string, any> {
 	name: string;
-	defaultvalue: number;
-	displayName: string;
-	description: string;
-	icon: string;
+	defaultvalue?: number;
+	displayName?: string;
+	description?: string;
+	icon?: string;
 	pullDate: Date;
 }
